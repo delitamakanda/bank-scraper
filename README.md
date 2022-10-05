cbfcae189c155cdcd410db0c45e36e105ed3f812
Délita Makanda <delita.makanda@gmail.com>
2022-10-05 23:03:34 +0200

    feat: scaper fortuneo #wip

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ source bankscraper/bin/activate
 scrape fortuneo.fr
 
 ```bash
-python3 scrapers/scraper_fortuneo.py <client_secret> <secret_code>
+python3 scrapers/scraper.py <client_secret> <secret_code> headless # mode debug
 ```
 
 ```bash
---
+++ scrapers/scraper.py
@@ -0,0 +1,84 @@
+import chromedriver_autoinstaller
+import sys
+import datetime
+from time import time, sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--username")
+    options.add_argument("--password")
+    options.add_argument("--headless")
+    if len(sys.argv) > 3  and sys.argv[3] == "headless":
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    else:
+        chromedriver_autoinstaller.install()
+        sleep(2)
+        browser = webdriver.Chrome(chrome_options=options)
+    return browser
+
+def run_fortuneo(browser):
+    base_url = "https://mabanque.fortuneo.fr/fr/identification.jsp"
+    browser.get(base_url)
+    browser.maximize_window()
+    
+    WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.ID, 'acces_client'))
+    )
+    scrape()
+
+
+def scrape():
+
+    account_number = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="loginContainer"]/input[1]')))
+
+    account_number.send_keys(sys.argv[1])
+
+    secret_code = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="passwordContainer"]/span[1]/input')))
+    secret_code.send_keys(sys.argv[2])
+
+    cookies_banner = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popin_tc_privacy_button_2"]')))
+    cookies_banner.click()
+
+    login_submit = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="valider_login"]')))
+
+    login_submit.click()
+
+    html = browser.page_source
+    output = parse_html(html)
+    print(output)
+
+
+def parse_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    print(soup.prettify())
+    account_synthese = []
+    return account_synthese
+
+
+if __name__ == "__main__":
+    print('my username', sys.argv[1])
+    print('my secret number', sys.argv[2])
+
+    start_time = time()
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    browser = get_driver()
+
+    run_fortuneo(browser)
+    
+    # Close the browser
+    browser.quit()
+    end_time = time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
+
+
